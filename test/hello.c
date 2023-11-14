e6585d303a840a98ca309289d67bb61c5aa111ae
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-11-14 12:27:39 +0200

    d

---
+++ test/hello.c
@@ -0,0 +1,29 @@
+#include "tst.h"
+
+
+void run(char **argv) {
+    int stat;
+    pid_t child_pid;
+
+    if (!argv || !argv[0])
+        return;
+
+    if (strcmp(argv[0], "ls") == 0 && (argv[1] == NULL || strcmp(argv[1], "-l") == 0)) {
+        child_pid = fork();
+        if (child_pid == -1) {
+            perror("fork");
+        } else if (child_pid == 0) {
+            // Child process
+            if (argv[1] == NULL) {
+                execlp("ls", "ls", (char *)NULL);
+            } else {
+                execlp("ls", "ls", "-l", (char *)NULL);
+            }
+            perror("ls");
+            exit(EXIT_FAILURE);
+        } else {
+            wait(&stat);
+        }
+        return;
+    }
+}
