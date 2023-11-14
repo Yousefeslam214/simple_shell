88822273e54881e754de9963e9c91d4fa48d07be
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-11-14 12:10:41 +0200

    f

--- runFun.c
+++ runFun.c
@@ -1,28 +1,57 @@
 #include "shell.h"
 
-void run(char **argv) {
-    int stat;
-    pid_t child_pid;
+void run(char **argv)
+{
+	int stat;
+	pid_t child_pid;
+	
+	if (!argv || !argv[0])
+		return;
 
-    if (!argv || !argv[0])
-        return;
+	if (strcmp(argv[0], "exit") == 0)
+	{
+		return;
+	}
+	else if (strcmp(argv[0], "ls") == 0)
+	{
 
-    if (strcmp(argv[0], "ls") == 0 && (argv[1] == NULL || strcmp(argv[1], "-l") == 0)) {
-        child_pid = fork();
-        if (child_pid == -1) {
-            perror("fork");
-        } else if (child_pid == 0) {
-            // Child process
-            if (argv[1] == NULL) {
-                execlp("ls", "ls", (char *)NULL);
-            } else {
-                execlp("ls", "ls", "-l", (char *)NULL);
-            }
-            perror("ls");
-            exit(EXIT_FAILURE);
-        } else {
-            wait(&stat);
-        }
-        return;
-    }
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("fork");
+		}
+		else if (child_pid == 0)
+		{
+
+			if (execvp("ls", argv) == -1)
+			{
+				perror("ls");
+			}
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(&stat);
+		}
+		return;
+	}
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("");
+	}
+	else if (child_pid == 0)
+	{
+
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror(argv[0]);
+		}
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&stat);
+	}
 }
